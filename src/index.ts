29e811283fca4e668428727e6936962c364f97b7
Harry Reyes <harryenthe13@gmail.com>
2024-10-06 03:00:58 +0800

    BETA Testing

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,24 @@ function checkChat(content: any) {
   return (filterChats as any)?.[content.type]?.some((chat: string) => content.chatmessage.includes(chat))
 }
 
+(window as any).observeChat = (node: HTMLDivElement) => {
+  const root = urlParams.get('rootObserver')
+  if (urlParams.has('observer')) {
+    console.log("root", root)
+    console.log("root", root)
+    let observer = new IntersectionObserver((entries) => {
+      const [entry] = entries
+      console.log("entry", entry)
+      node.style.visibility = entry.isIntersecting ? "visible" : "hidden"
+    }, {
+      root: root ? document.querySelector(`#${root}`) : null,
+      rootMargin: "0px",
+      threshold: 0.99
+    });
+    observer.observe(node as any);
+  }
+}
+
 function processData({ contents }: any) {
   const content = contents
   var node = document.createElement("div");
@@ -43,17 +61,9 @@ function processData({ contents }: any) {
 
   mountNode.appendChild(node);
 
+  (window as any).observeChat(node)
 
 
-  let observer = new IntersectionObserver((entries) => {
-    const [entry] = entries
-    node.style.visibility = entry.isIntersecting ? "visible" : "hidden"
-  }, {
-    root: mountNode,
-    rootMargin: "0px",
-    threshold: 0.99
-  });
-  observer.observe(node as any);
 }
 
 (window as any).processData = processData;
